3c93653d0e8ba12bd8183338b14f278fc0ece176
Flavio Moreira <flavio.moreira@outlook.com>
2019-12-27 20:39:54 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -16,3 +16,23 @@ var dataAtual = function () {
   mesNum = dataAtual.getMonth();
   return dataAtual.getDate() + ' de ' + (mesNome[mesNum]) + ' de ' + dataAtual.getFullYear();
 }
+
+document.addEventListener('click', function (e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement;
+
+    if (target.hasAttribute('data-toggle') && target.getAttribute('data-toggle') == 'modal') {
+        if (target.hasAttribute('data-target')) {
+            var m_ID = target.getAttribute('data-target');
+            document.getElementById(m_ID).classList.add('open');
+            e.preventDefault();
+        }
+    }
+
+    // Close modal window with 'data-dismiss' attribute or when the backdrop is clicked
+    if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'modal') || target.classList.contains('modal')) {
+        var modal = document.querySelector('[class="modal open"]');
+        modal.classList.remove('open');
+        e.preventDefault();
+    }
+}, false);
